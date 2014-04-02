c23b65c9ad87cc107487d5bd6175d7d3794aadd2
fmartinez255@gmail.com <fmartinez255@gmail.com@4d7622c6-eb8b-1262-4fd8-b7c52a78a604>
2014-04-02 22:28:11 +0000

    - Implemented very crude collision detection. Collisions are checked in GameWorld.java currently.
    - Rectangles (and appropriate getters) have been added to Rocket.java and Scrollable.java. These are used in collision detection
    
    - Added getMiddleX() and getMiddleY() in Rocket.java. These functions return the X and Y position in the middle of the sprite (instead of returning at the left side of the sprite)
    
    - Updated touch code in InputHandler. The player's sprite is now centered on the point of touch instead of being off to the side.

--- trunk/RocketAnimals/src/com/me/GameWorld/GameWorld.java
+++ trunk/RocketAnimals/src/com/me/GameWorld/GameWorld.java
@@ -1,22 +1,37 @@
 package com.me.GameWorld;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.Array;
+import com.me.GameObjects.AbstractObstacle;
 import com.me.GameObjects.Rocket;
+import com.me.GameObjects.Scrollable;
 import com.me.GameObjects.ScrollableHandler;
 
 public class GameWorld {
 	
 	private Rocket rocket;
 	private ScrollableHandler scroller;
+	private Array<AbstractObstacle> scrollObjects;
 	
 	public GameWorld(int midPointY) {
 		rocket = new Rocket((Gdx.graphics.getWidth() / 2) - 15, 400, 30, 30);
 		scroller = new ScrollableHandler();
+		scrollObjects = new Array<AbstractObstacle>();
 	}
 	
 	public void update(float delta) {
 		rocket.update(delta);
 		scroller.update(delta);
+		
+		// Very crude collision detection
+		scrollObjects = scroller.getAbstractObstacles();
+		for (Scrollable tempObj : scrollObjects) {
+			if (rocket.getRect().overlaps(tempObj.getRect())) {
+				scroller.removeObject((AbstractObstacle)tempObj);
+			}
+		}
+		
+		
 	}
 	
 	public Rocket getRocket() { return rocket; }
