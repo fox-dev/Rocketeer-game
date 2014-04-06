b7a6afac64f171f1c0e1671e2083e81897fbc8d6
fmartinez255@gmail.com <fmartinez255@gmail.com@4d7622c6-eb8b-1262-4fd8-b7c52a78a604>
2014-04-06 03:15:34 +0000

    Forgot to commit this class. Scaling should work now

---
+++ RocketAnimals/src/com/me/helpers/Constants.java
@@ -0,0 +1,10 @@
+package com.me.helpers;
+
+// Please define all your constant stuff here
+
+public class Constants {
+	
+	public static final int TRUE_WIDTH = 320; // Game's "true" width = 320px
+	public static final int TRUE_HEIGHT = 480; // Game's "true" height = 480px
+	public static final float ASPECT_RATIO = (float)TRUE_WIDTH / (float)TRUE_HEIGHT; 
+}
