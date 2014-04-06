3316da344cd250d0ba139cf2efc133ac2501114b
aabriam@gmail.com <aabriam@gmail.com@4d7622c6-eb8b-1262-4fd8-b7c52a78a604>
2014-04-06 03:57:26 +0000

    Added a camera in the input handler to translate phone screen positions to gameworld positions.

--- RocketAnimals/src/com/me/helpers/InputHandler.java
+++ RocketAnimals/src/com/me/helpers/InputHandler.java
@@ -2,27 +2,42 @@ package com.me.helpers;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector3;
 import com.me.GameObjects.Rocket;
 
 public class InputHandler implements InputProcessor {
 	
 	private Rocket rocket;
 	
+	private OrthographicCamera cam;
+	
+	
+	
 	public InputHandler(Rocket rocket) {
+		
 		this.rocket = rocket;
+		
+		cam = new OrthographicCamera();
+		cam.setToOrtho(true, Constants.TRUE_WIDTH, Constants.TRUE_HEIGHT);
+		
+		
 	}
 
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		System.out.println("Touched " + screenX);
-		if (screenX < rocket.getMiddleX()) {
+		
+		Vector3 tempPos = new Vector3(screenX, screenY, 0);
+		cam.unproject(tempPos);
+		System.out.println("Touched " + tempPos.x);
+		if (tempPos.x < rocket.getMiddleX()) {
 			
 			rocket.onLeft();
-			rocket.userAtX(screenX);
+			rocket.userAtX((int)tempPos.x);
 		}
 		else
 			rocket.onRight();
-		    rocket.userAtX(screenX);
+		    rocket.userAtX((int)tempPos.x);
 		return true;
 	}
 
@@ -51,18 +66,20 @@ public class InputHandler implements InputProcessor {
 	public boolean touchDragged(int screenX, int screenY, int pointer) {
 		
 		
+		Vector3 tempPos = new Vector3(screenX, screenY, 0);
+		cam.unproject(tempPos);
+		System.out.println("Touched " + tempPos.x);
 		
-		
-		if (screenX < rocket.getMiddleX()) {
+		if (tempPos.x < rocket.getMiddleX()) {
 			
 			rocket.onLeft();
-			rocket.userAtX(screenX);
+			rocket.userAtX((int)tempPos.x);
 			
 			
 		}
 		else {
 			rocket.onRight();
-			rocket.userAtX(screenX);
+			rocket.userAtX((int)tempPos.x);
 		}
 		
 		
