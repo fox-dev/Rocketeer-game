4a85d20a364f11799f043c4a97fb864cda3d167b
fmartinez255@gmail.com <fmartinez255@gmail.com@4d7622c6-eb8b-1262-4fd8-b7c52a78a604>
2014-04-13 02:46:49 +0000

    - Added game states. Currently the game starts in a "ready" state

--- trunk/RocketAnimals/src/com/me/GameWorld/GameWorld.java
+++ trunk/RocketAnimals/src/com/me/GameWorld/GameWorld.java
@@ -1,6 +1,5 @@
 package com.me.GameWorld;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.Array;
 import com.me.GameObjects.AbstractObstacle;
 import com.me.GameObjects.Rocket;
@@ -14,13 +13,43 @@ public class GameWorld {
 	private ScrollableHandler scroller;
 	private Array<AbstractObstacle> scrollObjects;
 	
+	private GameState currentState;
+	
+	// Game states
+	public enum GameState {
+		READY, RUNNING, GAMEOVER
+	}
+	
 	public GameWorld(int midPointY) {
+		currentState = GameState.READY;
 		rocket = new Rocket((Constants.TRUE_WIDTH / 2) - 15, 400, 30, 30);
 		scroller = new ScrollableHandler();
 		scrollObjects = new Array<AbstractObstacle>();
 	}
 	
 	public void update(float delta) {
+		
+		switch(currentState) {
+		
+		case READY:
+			updateReady(delta);
+			break;
+			
+		case RUNNING:
+			updateRunning(delta);
+			break;
+			
+		default:
+			break;
+		}
+		
+	}
+	
+	private void updateReady(float delta) {
+		rocket.update(delta);
+	}
+	
+	private void updateRunning(float delta) {
 		rocket.update(delta);
 		scroller.update(delta);
 		
@@ -31,12 +60,27 @@ public class GameWorld {
 				scroller.removeObject((AbstractObstacle)tempObj);
 			}
 		}
-		
-		
 	}
 	
-	public Rocket getRocket() { return rocket; }
+	public void restart() {
+		;
+	}
 	
+	// Game state setters
+	public void ready() {
+		currentState = GameState.READY;
+	}
+	
+	public void start() {
+		currentState = GameState.RUNNING;
+	}
+	
+	// Game state getters
+	public boolean isReady() { return currentState == GameState.READY; }
+	public boolean isRunning() { return currentState == GameState.RUNNING; }
+	public boolean isGameOver() { return currentState == GameState.GAMEOVER; }
+	
+	public Rocket getRocket() { return rocket; }
 	public ScrollableHandler getScroller() { return scroller; }
 	
 }
