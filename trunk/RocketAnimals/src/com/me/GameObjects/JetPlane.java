dd10f4639cad86d5fca6183e0f6b028b15ae9872
aabriam@gmail.com <aabriam@gmail.com@4d7622c6-eb8b-1262-4fd8-b7c52a78a604>
2014-04-22 23:25:01 +0000

    Added event flag for jetplanes in scrollhandler, had jet plane spawn randomly from right side of screen.

--- trunk/RocketAnimals/src/com/me/GameObjects/JetPlane.java
+++ trunk/RocketAnimals/src/com/me/GameObjects/JetPlane.java
@@ -2,23 +2,28 @@ package com.me.GameObjects;
 
 import com.badlogic.gdx.math.Rectangle;
 
+
 public class JetPlane extends AbstractObstacle
 {
 	protected Rectangle collisionRect;
+	protected boolean isSpawned;
 	
 	public JetPlane(float x, float y, int width, int height, float scrollSpeed)
 	{
 		super(x, y, width, height, scrollSpeed);
 		collisionRect = new Rectangle(x, y, width, height);
+		velocity.x = -200;
+		velocity.y = 80;
 	}
 	
 	public void update(float delta)
 	{
 		position.add(velocity.cpy().scl(delta));
 		
-		if (position.y > 480 || position.x < 0)
+		if (position.y > 480 || position.x + width < 0)
 		{
 			isScrolledDown = true;
+			
 		}
 		
 		collisionRect.setPosition(position);
